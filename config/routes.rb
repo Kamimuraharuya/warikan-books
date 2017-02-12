5d49c4efb3ac7584b6a7064824f1ed0c0696b7fa
上村治也 <haruyasora423@gmail.com>
2017-02-12 12:18:17 +0900

    とりあえず適当

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  get "logout" => "sessions#destroy"
+  
+
+    get "signup" => "users#new"
+
 	post "signup" => "users#create" #エラー時に良くわからん
 	get "signup" => "users#new"
 
