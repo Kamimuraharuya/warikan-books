073bbe07ddc9413b2d03904d78f3fb851d37190f
haruya <haruyasora423@gmail.com>
2017-03-04 11:41:04 +0900

    fogが良くなかったのかもしれない、とおもい、分岐消してみた

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,11 +10,11 @@ include Cloudinary::CarrierWave
   #storage :file
   # storage :fog
 
-if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+#if Rails.env.production?
+ #   storage :fog
+  #else
+   # storage :file
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
