df8b16e2204607de95dc8f6d6a1aabfe3e0e9b00
上村治也 <haruyasora423@gmail.com>
2017-03-06 16:04:35 +0900

    微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   validates :image, presence: true  
   validate :image_size
   validates :password, presence: false, on: :facebook_login
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, :major, :university, :favorite_book, presence: true, length: { maximum: 140 }
 
   def self.from_omniauth(auth)
         # emailの提供は必須とする
